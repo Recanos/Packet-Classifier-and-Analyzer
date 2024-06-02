5dd46bb4f5e864b2686bd6ddc61d9c3689187d83
Nikita <recanos@yandex.ru>
2024-06-02 20:54:00 +0300

    little corrections

--- cpp_program_2/src/main.cpp
+++ cpp_program_2/src/main.cpp
@@ -3,7 +3,7 @@
 int main() {
     setlocale(LC_ALL, "Russian");
     DataProcessor processor;
-    if (processor.processFile("../../python_program_1/data.csv")) {
+    if (processor.processFile("../../../python_program_1/data.csv")) {
         processor.writeOutput("output.csv");
     }
 
